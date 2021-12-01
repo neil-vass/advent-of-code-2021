43882465bf14c5dd6346011ffbfbb22df99c9f05
Neil Vass <neil.vass+github@gmail.com>
2021-12-01 17:11:06 +0000

    Complete day 1 part 1

--- day01.py
+++ day01.py
@@ -4,19 +4,35 @@
 # There's lots of advice about best ways to set it up, but pip just works.
 
 def test_fetch_data_gives_ints():
-    data = fetch_data()
+    data = fetch_data('sample_data/day01.txt')
     assert data[0] == 199
     assert data[-1] == 263
-    
+
+def test_num_increases():
+    assert num_increases([199, 200]) == 1
+    assert num_increases([210, 200, 207, 240]) == 2
+
+def test_sample_data_gives_correct_result():
+    data = fetch_data('sample_data/day01.txt')
+    assert num_increases(data) == 7
 
 
-def fetch_data():
-    with open('small_inputs/day01.txt', 'r') as f:
+def fetch_data(path):
+    with open(path, 'r') as f:
         li = [int(x) for x in f]
     return li
 
-
+def num_increases(data):
+    increases = 0 
+    try:
+        for idx, x in enumerate(data):
+            if data[idx+1] > x:
+                increases += 1
+    except IndexError:
+        pass
+    return increases
 
 
 if __name__ == "__main__":
-    print('Hello, World!')
+    data = fetch_data('data/day01.txt')
+    print(num_increases(data))
