7fb097cc3e3b7a9871e7b68fd95d3ec249363b6b
Neil Vass <neil.vass+github@gmail.com>
2021-12-03 09:01:54 +0000

    Helper funtion

--- day00.py
+++ day00.py
@@ -3,6 +3,10 @@ def test_basics():
     assert 1 == 0
 
 
+def fetch_data(path):
+    with open(path, 'r') as f:
+        for ln in f:
+            yield ln
 
 if __name__ == "__main__":
     print('Hello, World!')
