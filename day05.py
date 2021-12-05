9d6957e285047d14dbf17f6e6c39b7b95fdd7c2b
Neil Vass <neil.vass+github@gmail.com>
2021-12-05 10:22:49 +0000

    Completed day 5 part 2

--- day05.py
+++ day05.py
@@ -1,3 +1,5 @@
+# Could probably just keep a dictionary of what cooridnates aren't zero - would mean we don't need to 
+# specify a field size at the start. But, I like practicing numpy so I've used an array.
 import numpy as np
 import re
 
@@ -9,13 +11,22 @@ class Field:
         if x1 == x2:
             min_y = min(y1, y2)
             max_y = max(y1, y2) +1
-            self.matrix[x1,...][min_y:max_y] +=1
+            self.matrix[x1,...][min_y:max_y] += 1
         elif y1 == y2:
             min_x = min(x1, x2)
             max_x = max(x1, x2) +1
-            self.matrix[min_x:max_x][...,y1] +=1
+            self.matrix[min_x:max_x][...,y1] += 1
         else:
-            pass
+            # 45 degree diagonal
+            cur_x, cur_y = x1, y1
+            x_step = 1 if x2 > x1 else -1 
+            y_step = 1 if y2 > y1 else -1
+            self.matrix[cur_x, cur_y] += 1
+            while cur_x != x2:
+                cur_x += x_step
+                cur_y += y_step
+                self.matrix[cur_x, cur_y] += 1
+
 
     def mark_all(self, data):
         for entry in data:
@@ -48,10 +59,13 @@ def test_line_with_same_y_vals():
     assert field.matrix[7,7] == 1
     assert field.matrix.sum() == 3
 
-def test_line_without_matching_x_or_y_is_ignored():
+def test_diagonal_line_is_marked():
     field = Field(10, 10)
-    field.mark(8,0, 0,8)
-    assert field.matrix.sum() == 0
+    field.mark(1,1, 3,3)
+    assert field.matrix[1,1] == 1
+    assert field.matrix[2,2] == 1
+    assert field.matrix[3,3] == 1
+    assert field.matrix.sum() == 3
 
 def test_overlaps():
     field = Field(10, 10)
@@ -68,7 +82,7 @@ def test_sample_data_gives_correct_result():
     field = Field(10, 10)
     data = fetch_data('sample_data/day05.txt')
     field.mark_all(data)
-    assert field.count_overlaps() == 5
+    assert field.count_overlaps() == 12
 
 
 #-----------------------------------------------------#
